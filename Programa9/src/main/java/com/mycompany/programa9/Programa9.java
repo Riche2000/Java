28252ba4650d713b068e1de92f4e6956d590ce1d
riche2000 <ricardosanch2000@gmail.com>
2023-01-06 17:59:59 -0600

    Operadores Aritmeticos combinados con asignación

---
+++ Programa9/src/main/java/com/mycompany/programa9/Programa9.java
@@ -0,0 +1,24 @@
+package com.mycompany.programa9;
+
+public class Programa9 {
+
+    public static void main(String[] args) {
+        //Operadores Aritmeticos combinados con asignación
+        
+        int numero = 10;
+        
+      //numero = numero + 5;
+      //numero += 5; //Combinacion de operadores +=
+      
+      //numero = numero -5;
+      //numero -= 5;
+      
+      //numero *= 5;
+      
+      //numero /= 5;
+      
+      numero %= 5;
+      
+        System.out.println(numero);
+    }
+}
