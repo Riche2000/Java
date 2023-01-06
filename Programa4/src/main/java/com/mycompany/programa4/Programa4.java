512d4ff9c32782d86473ac304f68c339d7c229af
riche2000 <ricardosanch2000@gmail.com>
2023-01-06 12:14:15 -0600

    DATOS NO PRIMITIVOS Y DECLARAR CADENAS DE CARACTERES

---
+++ Programa4/src/main/java/com/mycompany/programa4/Programa4.java
@@ -0,0 +1,17 @@
+package com.mycompany.programa4;
+
+public class Programa4 {
+
+    public static void main(String[] args) {
+        //DATOS NO PRIMITIVOS Y DECLARAR CADENAS DE CARACTERES
+        //Integer es un tipo de dato no primitivo nos permite almacenar enteros y null
+        Integer numero = null; //null significa que el pedazo de memoria esta vacio 
+        //Metodos Por ejemplo numero. Metodo
+        
+        //CADENAS
+        String palabra = "Hola que tal?";
+        
+        System.out.println("El numero es: "+numero);
+        System.out.println(palabra);
+    }
+}
