21f71e693176698d78fe19114234d67357aeafb1
riche2000 <ricardosanch2000@gmail.com>
2023-01-06 12:37:25 -0600

    Constantes

---
+++ Programa5/src/main/java/com/mycompany/programa5/Programa5.java
@@ -0,0 +1,13 @@
+package com.mycompany.programa5;
+
+public class Programa5 {
+
+    public static void main(String[] args) {
+        //CONSTANTES
+        /*Es un espacion en memoria donde puedes alamacenar un valor
+          pero el valor no va a cambiar a lo largo del programa es decir 
+          el valor que se asigne al inicio no se va a cambiar a lo largo del proograma*/
+        final int numero= 10; //Se declara la constante con final
+        System.out.println(numero);
+    }
+}
---
+++ Programa6/src/main/java/com/mycompany/programa6/Programa6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa6;
+
+/**
+ *
+ * @author yo789
+ */
+public class Programa6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
