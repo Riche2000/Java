2bd62e4d539f96ce472f77f2d946deb8f14a871b
riche2000 <ricardosanch2000@gmail.com>
2023-01-06 11:07:02 -0600

    Programa 1

---
+++ Programa1/src/main/java/com/mycompany/programa1/Programa1.java
@@ -0,0 +1,14 @@
+package com.mycompany.programa1;
+
+public class Programa1 {
+    
+    //Esta es nuestra función principal la que dice main
+    public static void main(String[] args) {
+        //System.out.println(""); Es la sintaxis para imprimir en panatalla con salto de linea
+        System.out.println("Hola mundo");
+        //Para que se ponga solo el imprimir pantalla ponemos sout y tabulador
+        System.out.println("sout Tab");
+        //System.out.print(""); Es la sintaxis para imprimir en panatalla
+        System.out.print("Hola Mundo");
+    }
+}
