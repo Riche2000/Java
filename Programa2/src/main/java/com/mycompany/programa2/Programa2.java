5473c1d4e5a3a66538eb2b329c2e423f45aceac5
riche2000 <ricardosanch2000@gmail.com>
2023-01-06 11:14:59 -0600

    Comentarios

---
+++ Programa2/src/main/java/com/mycompany/programa2/Programa2.java
@@ -0,0 +1,15 @@
+package com.mycompany.programa2;
+
+public class Programa2 {
+
+    public static void main(String[] args) {
+        //Los comentarios de una linea
+        /*
+            Comentario
+            De
+            Varias
+            Lineas
+         */
+        System.out.println("Los Comenatarios");
+    }
+}
---
+++ Programa3/src/main/java/com/mycompany/programa3/Programa3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa3;
+
+/**
+ *
+ * @author yo789
+ */
+public class Programa3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
