3fead4f10ddbd0090f61257b1739548089a3d198
riche2000 <ricardosanch2000@gmail.com>
2023-01-06 18:49:46 -0600

    Calcular la suma de 3 calificaciones

--- Programa12/src/main/java/com/mycompany/programa12/Programa12.java
+++ Programa12/src/main/java/com/mycompany/programa12/Programa12.java
@@ -1,16 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.programa12;
 
-/**
- *
- * @author yo789
- */
+import java.util.Scanner;
+
 public class Programa12 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Calcular la suma de 3 calificaciones
+        Scanner entrada = new Scanner(System.in);
+        float nota1,nota2,nota3,suma;
+        
+        System.out.println("Digita 3 Calificacioens: ");
+        
+        //Guardamos las 3 calificaciones
+        nota1 = entrada.nextFloat();
+        nota2 = entrada.nextFloat();
+        nota3 = entrada.nextFloat();
+        
+        suma = nota1 + nota2 + nota3; //Sumamos las 3 notas
+        
+        System.out.println("\n El resultado de la suma es: "+suma); // el \n da un salto de linea 
     }
 }
