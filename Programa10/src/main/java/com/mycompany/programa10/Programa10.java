bf034942a8213feabf5d81baf5290776de7c3bad
riche2000 <ricardosanch2000@gmail.com>
2023-01-06 18:14:26 -0600

    Operador de incremento y decremento

---
+++ Programa10/src/main/java/com/mycompany/programa10/Programa10.java
@@ -0,0 +1,17 @@
+package com.mycompany.programa10;
+
+public class Programa10 {
+
+    public static void main(String[] args) {
+        //OPERADORES DE INCREMENTO Y DECREMENTO
+        
+        //x++; incremento Esto equivale a x = x + 1;
+        //x--;  Decremento Esto equivale a x = x - 1;
+        
+        int x = 5, y;
+        // y = x++; Primero asigna a y despues aumenta x+1
+        y = ++x; //Incrementa el valor de x 1 y despues se lo asigna a y
+        
+        System.out.println(y);
+    }
+}
