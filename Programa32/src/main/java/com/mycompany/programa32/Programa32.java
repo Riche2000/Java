2e7ddc4abaa9237c6bca037ba6e85b91c18d351c
riche2000 <ricardosanch2000@gmail.com>
2023-01-09 13:13:11 -0600

    Ciclos - Mostrar los números del 100 al 0 de 7 en 7

---
+++ Programa32/src/main/java/com/mycompany/programa32/Programa32.java
@@ -0,0 +1,12 @@
+package com.mycompany.programa32;
+
+public class Programa32 {
+
+    public static void main(String[] args) {
+        //Ciclos - Mostrar los números del 100 al 0 de 7 en 7
+        for(int i = 100; i >= 0; i -= 7)
+        {
+            System.out.println(i);
+        }
+    }
+}
