663be532a085554f0506f095e72d142dab8cbcb4
riche2000 <ricardosanch2000@gmail.com>
2023-01-14 10:07:32 -0600

    POO - Creación de clases y objetos

--- Programa59/src/ClasesYObjetos/Coche.java
+++ Programa59/src/ClasesYObjetos/Coche.java
@@ -24,6 +24,18 @@ public class Coche {
         System.out.println("El color del coche1 es: "+coche1.color);
         System.out.println("La marca del coche1 es: "+coche1.marca);
         System.out.println("El kilometraje del coche1 es: "+coche1.km);
+        
+        //Creamos un segundo objeto
+        Coche coche2 = new Coche();
+        
+        //Llenamos los atributos del objeto
+        coche2.color = "Rojo";
+        coche2.marca = "Ferrari";
+        coche2.km = 100;
+        
+        System.out.println("\nEl color del coche2 es: "+coche2.color);
+        System.out.println("La marca del coche2 es: "+coche2.marca);
+        System.out.println("El kilometraje del coche2 es: "+coche2.km);
     }
     
 }
