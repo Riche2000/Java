5a1a59c57bf9b4879e70e91de1e3a26d96df7428
riche2000 <ricardosanch2000@gmail.com>
2023-01-13 20:11:21 -0600

    POO - Conceptos de Programación Orientada a Objetos (POO)

---
+++ Programa59/src/main/java/com/mycompany/programa59/Programa59.java
@@ -0,0 +1,8 @@
+package com.mycompany.programa59;
+
+public class Programa59 {
+
+    public static void main(String[] args) {
+        //POO - Conceptos de Programación Orientada a Objetos (POO)
+    }
+}
